cae5d2c8c0d220810adf9df2ad7fb5054052f43d
mariusfa <mariusfagerland@yahoo.no>
2022-07-23 17:39:05 +0200

    Create Team controller

---
+++ src/main/kotlin/com/fagerland/freeleashadminapi/team/TeamController.kt
@@ -0,0 +1,23 @@
+package com.fagerland.freeleashadminapi.team
+
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController("/team")
+class TeamController {
+
+    @GetMapping
+    fun listTeams() = "TODO"
+
+    @PostMapping
+    fun createTeam() = "TODO"
+
+    @PutMapping
+    fun updateTeam() = "TODO"
+
+    @DeleteMapping
+    fun deleteTeam() = "TODO"
+}
