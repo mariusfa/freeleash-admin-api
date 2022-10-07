f3789e8304fc3f56ff2acc326b48746fcf456c35
mariusfa <mariusfagerland@yahoo.no>
2022-10-08 00:04:46 +0200

    Create postman file and fix delete

--- src/main/kotlin/com/fagerland/freeleashadminapi/toggle/ToggleRepository.kt
+++ src/main/kotlin/com/fagerland/freeleashadminapi/toggle/ToggleRepository.kt
@@ -1,11 +1,13 @@
 package com.fagerland.freeleashadminapi.toggle
 
 import org.springframework.data.jpa.repository.JpaRepository
+import javax.transaction.Transactional
 
 interface ToggleRepository: JpaRepository<Toggle, Long> {
     fun existsByNameAndTeamId(name: String, teamId: Long): Boolean
 
     fun findAllByTeamName(name: String): List<Toggle>
 
+    @Transactional
     fun deleteAllByTeamId(id: Long)
 }
