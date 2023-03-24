3f00bc2006658cec1ca876583c929b86ad0a089b
mariusfa <mariusfagerland@yahoo.no>
2023-03-24 21:01:21 +0100

    Create datajpatest

---
+++ src/test/kotlin/com/fagerland/freeleashadminapi/TeamRepositoryTest.kt
@@ -0,0 +1,38 @@
+package com.fagerland.freeleashadminapi
+
+import com.fagerland.freeleashadminapi.team.Team
+import com.fagerland.freeleashadminapi.team.TeamRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.jdbc.core.JdbcTemplate
+import javax.persistence.EntityManager
+
+@DataJpaTest
+class TeamRepositoryTest {
+
+    @Autowired
+    private lateinit var teamRepository: TeamRepository
+
+    @Autowired
+    private lateinit var jdbcTemplate: JdbcTemplate
+
+    @Test
+    fun `should inject components`() {
+        assertThat(teamRepository).isNotNull
+        assertThat(jdbcTemplate).isNotNull
+    }
+
+    @Test
+    fun `should be able to create team`() {
+        teamRepository.save(Team(name = "testing"))
+        //val listOfTeams: List<Team> = entityManager.createQuery("select t from Team t").resultList as List<Team>
+        val listOfTeams = jdbcTemplate.queryForList("select * from team")
+        assertThat(listOfTeams).hasSize(1)
+        val teamFound = listOfTeams[0]
+        assertThat(teamFound["NAME"]).isEqualTo("testing")
+    }
+
+}
