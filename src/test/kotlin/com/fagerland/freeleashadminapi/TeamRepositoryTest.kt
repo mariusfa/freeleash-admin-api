266b2b38bf012e18a0d830f857d11fa3fbd01ab9
mariusfa <mariusfagerland@yahoo.no>
2023-03-25 15:31:56 +0100

    Use testcontainers in repository tests

--- src/test/kotlin/com/fagerland/freeleashadminapi/TeamRepositoryTest.kt
+++ src/test/kotlin/com/fagerland/freeleashadminapi/TeamRepositoryTest.kt
@@ -7,8 +7,12 @@ import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.jdbc.core.JdbcTemplate
+import org.springframework.test.context.ActiveProfiles
+import org.testcontainers.junit.jupiter.Testcontainers
 
 @DataJpaTest
+@ActiveProfiles("test")
+@Testcontainers
 class TeamRepositoryTest {
 
     @Autowired
